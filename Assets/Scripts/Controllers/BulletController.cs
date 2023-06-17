f1ef422b1de5772ff12dd0ec79cc1326d00725a0
ArneKlages4444 <aklages@uni-osnabrueck.de>
2023-06-17 16:28:46 +0200

    refactor

---
+++ Assets/Scripts/Controllers/BulletController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Controllers
+{
+    public class BulletController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody RB;
+
+        [SerializeField] private float bulletSpeed = 10f;
+        private float _lifeSpan = 4f;
+        private Rigidbody _rigidbody;
+        private Vector3 _direction;
+
+        [SerializeField] private LayerMask layersToHit;
+
+        private void Start()
+        {
+            var mousePos = Input.mousePosition;
+            // mousePos.z = Camera.main.nearClipPlane + 1;
+            // mousePos = Camera.main.ScreenToWorldPoint((mousePos));
+            // var rigidbodyPlayer = GameObject.Find("Player").GetComponent<Rigidbody>();
+            var targetPos = Vector3.zero;
+            var ray = Camera.main.ScreenPointToRay(mousePos);
+            if (Physics.Raycast(ray, out var hitData, 50, layersToHit))
+            {
+                targetPos = hitData.point;
+            }
+
+            _rigidbody = GetComponent<Rigidbody>();
+            var position = _rigidbody.position;
+            _direction = (targetPos - position).normalized;
+        }
+
+        public void Update()
+        {
+            // SHOOT BULLET
+            transform.Translate(_direction * (bulletSpeed * Time.deltaTime));
+        }
+    }
+}
